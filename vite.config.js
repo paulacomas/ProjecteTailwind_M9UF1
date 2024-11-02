c7eca363f6fffdd1ddb5f3d082d44b39b162ef69
Paula <paula.comas@cirvianum.cat>
2024-11-02 20:30:04 +0100

    config vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,39 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-  root: "./", // Root directory for Vite
-  base: "./", // Use relative paths for assets
+  root: "./", // Mantiene el root en la raíz del proyecto
+  base: "./", // Usa rutas relativas para los recursos
   build: {
-    outDir: "dist", // Output directory for the build
+    outDir: "dist", // Directorio de salida para la construcción
     rollupOptions: {
-      input: "./index.html", // Entry point for the build
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        confirmar_proposta: path.resolve(
+          __dirname,
+          "src/pages/confirmar_proposta.html"
+        ),
+        detalls: path.resolve(__dirname, "src/pages/detalls.html"),
+        home: path.resolve(__dirname, "src/pages/home.html"),
+        mostrar_objectes: path.resolve(
+          __dirname,
+          "src/pages/mostrar_objectes.html"
+        ),
+        new_object: path.resolve(__dirname, "src/pages/new_object.html"),
+        perfil: path.resolve(__dirname, "src/pages/perfil.html"),
+        proposta_detalls: path.resolve(
+          __dirname,
+          "src/pages/proposta_detalls.html"
+        ),
+        register: path.resolve(__dirname, "src/pages/register.html"),
+        serch: path.resolve(__dirname, "src/pages/serch.html"),
+        xat: path.resolve(__dirname, "src/pages/xat.html"),
+      },
     },
   },
   server: {
     watch: {
-      usePolling: true, // Ensures changes are detected in some environments
+      usePolling: true,
     },
   },
 });
